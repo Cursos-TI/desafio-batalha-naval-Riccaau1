b8542aa53922f3332ee0bbc012aab6b22b9f7420
Riccaau1 <ricardosc.tec@gmail.com>
2025-04-29 01:11:49 +0000

    Mestre 100%

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define LINHA 10
+#define COLUNA 10
+
+int casoLosango(int x){
+    return (x < 0) ? -x : x;
+}
+
 
 int main() {
 
     char linha[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-
-    int tabuleiro[10][10];        
-        for (int i = 0; i < 10; i++){                    
-            for (int j = 0; j < 10; j++) {             
+    
+    int tabuleiro[LINHA][COLUNA];        
+        for (int i = 0; i < LINHA; i++){                    
+            for (int j = 0; j < COLUNA; j++) {             
                 tabuleiro[i][j] = 0;
             }        
         } 
@@ -35,17 +39,52 @@ int main() {
     for (int i = 0; i < 3; i++) {
         tabuleiro[i][2 - i] = 3;              
     }
+
+    int tamanhoCone = 3;
+    int linhaInicialCone = 5;
+    int colunaInicialCone = 8;
+
+    for (int i = 0; i < tamanhoCone; i++) {
+        int numeroLinhaCone = 2 * i + 1; 
+        int deslocamentoColunaCone = (colunaInicialCone - 1) + numeroLinhaCone / 2;
+        for (int j = 0; j < numeroLinhaCone; j++) {            
+            tabuleiro[linhaInicialCone + i][deslocamentoColunaCone - j] = 5;
+        }
+    }
+
+    int tamanhoCruz = 5;
+    int linhaInicialCruz = 1;
+    int colunaInicialCruz = 5;
+
+    for (int i = 0; i < tamanhoCruz; i++) {
+        tabuleiro[i + 1][colunaInicialCruz] = 5;
+        for (int j = 0; j < tamanhoCruz; j++) {
+            tabuleiro[linhaInicialCruz + 2][j + 3] = 5;
+        }        
+    }
+    
+    int centroLosangoLinha = 7;
+    int centroLosangoColuna = 2;
+    int tamanhoLosango = 5;
+
+    for (int i = -tamanhoLosango / 2; i <= tamanhoLosango / 2; i++) {
+        for (int j = -tamanhoLosango / 2; j <= tamanhoLosango / 2; j++) {
+            if (casoLosango(i) + casoLosango(j) <= tamanhoLosango / 2) {
+                tabuleiro[centroLosangoLinha + i][centroLosangoColuna + j] = 5;
+            }
+        }
+    } 
   
     printf("    **Jogo de Batalha Naval**\n");
         printf("   ");
-        for (int l = 0; l < 10; l++) {
+        for (int l = 0; l < LINHA; l++) {
             printf(" %c ", linha[l]);                                       
         }
         printf("\n");
         
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < LINHA; i++) {
             printf("%2d ", i + 1);
-            for (int j = 0; j < 10; j++) {
+            for (int j = 0; j < COLUNA; j++) {
                 printf(" %d ", tabuleiro[i][j]);
             }
             printf("\n");        
@@ -56,38 +95,6 @@ int main() {
     }
 
     
-    
 
-
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     
